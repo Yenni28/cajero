03e39a2ea35dc16443aa3896259031415eac9382
Yenni28 <yen.ni.28@hotmail.com>
2023-11-27 00:40:32 -0600

    Cajero

---
+++ index.js
@@ -0,0 +1,105 @@
+const screen = document.querySelector(".atm-system__screen");
+const inputCard = document.querySelector(".atm-system__card");
+
+// const key = document.querySelectorAll(".atm-system__keys");
+// key[0].addEventListener("click", (event) => console.log(event.target.id));
+// key[4].addEventListener("click", (event) => console.log(event.target.id));
+
+const keys = document.querySelector(".atm-system__input-keys");
+let pin = [];
+let state = "";
+
+const validatePin = (pin_keys) => {
+  if (pin_keys.toLocaleString().replaceAll(",", "") === "0000") {
+    // Convertimos el array a string y reemplazamos las , (comas) y validamos que el resultado sea igual a 0000
+    console.log(pin_keys)
+    screen.innerText = "Pin ingresado es correcto ahora presiona la tecla verde";
+    state = 'valido';
+  } else {
+    console.log(pin_keys)
+    screen.innerText = "Pin ingresado es incorrecto";
+  }
+};
+
+keys.addEventListener("click", (event) => {
+  const id = event.target.id;
+
+  if ((parseInt(id) || id === '0') && typeof parseInt(id) === 'number') {
+    // Sí tiene id va a entrar
+    // Recogemos
+     //el valor de el id y hacemos push al array pin
+    if (state === "login" && pin.length <= 4) {
+      pin.push(event.target.id);
+      
+      // Ejecutamos la función para validar el pin
+      if (pin.length === 4) {
+        // Validamos la longitud
+        validatePin(pin);
+      }
+    }
+
+  } else if (id === 'btn-cancel') { 
+    console.log('btn-cancel');
+    
+    if (state === 'login') {
+      pin = [];
+      screen.innerText = "Operacion cancelada";
+    }
+
+  } else if (id === 'btn-success') {
+    console.log('btn-success');
+    if (state === 'valido') {
+      screen.innerText = "Operacion en proceso";
+    }
+  } else if (id === 'btn-delete') { 
+    console.log('btn-delete');
+  
+    if (pin.length) {
+      pin.pop();
+      screen.innerText = "Digito eliminado";
+    }
+  } else {
+    console.log("Tecla sin función");
+  }
+});
+
+inputCard.addEventListener("click", function () {
+  screen.innerText = "Validando tarjeta...";
+  setTimeout(() => {
+    screen.innerText = "Ingrese su pin";
+    state = "login";
+  }, 2500);
+});
+
+window.addEventListener("load", () => {
+  screen.innerText = "Bienvenido al ATM";
+});
+
+// function awake(parametro) {
+//   console.log(parametro);
+// }
+
+// const sleep = (parametro) => {
+//   console.log(parametro);
+// };
+
+// const sum = (a, b) => {
+//   return a + b;
+// };
+
+// const sum = (a, b) => a + b;
+// const sum = (a, b) => (a + b);
+
+// awake("awake");
+// sleep("sleep");
+// sum(3, 5);
+
+// console.log(sum(4, 8))
+
+// PascalCase -> para clases
+// camelCase -> para variables
+// snake_case -> keys en un objeto
+
+// const arr = ['victor', 37]
+// const obj = {name: 'victor', age: 37}
+// console.log(obj.name, obj.age)
